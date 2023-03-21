08799135b4a0773db81e548f329fb4b0f7bdcb0e
Axel Fuzzi <axelfuzziii@gmail.com>
2023-03-21 11:28:06 -0300

    clusteer mode fixed

--- proyectoPrueba/src/app.js
+++ proyectoPrueba/src/app.js
@@ -1,11 +1,34 @@
 import express from 'express';
+import cluster from 'cluster';
+import http from 'http';
+import { cpus } from 'os';
 
-const app = express();
+//const app = express();
 
 const PORT = process.env.PORT ||8080;
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+const numCPUs = cpus().length;
 
-app.get('/',(req,res)=>{
-    res.send(`Petición atendida por ${process.pid} en ${PORT}`)
-})
+if (cluster.isPrimary){
+
+    for (let i = 0; i < numCPUs; i++){
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`)
+    })
+}else{
+    http.createServer((req, res) => {
+        res.writeHead(200);
+        res.end('hello word\n');
+    }).listen(8000);
+
+    console.log(`worker ${process.pid} started`);
+}
+
+//app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+
+//pp.get('/',(req,res)=>{
+//   res.send(`Petición atendida por ${process.pid} en ${PORT}`)
+//)
